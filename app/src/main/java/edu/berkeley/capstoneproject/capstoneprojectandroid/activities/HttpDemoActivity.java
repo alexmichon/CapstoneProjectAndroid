a301d3d16fae16b8a106e9cd5b3f7a954ad542d6
Alex <alexmichon@hotmail.fr>
2017-10-19 15:21:03 -0700

    Created files

---
+++ app/src/main/java/edu/berkeley/capstoneproject/capstoneprojectandroid/activities/HttpDemoActivity.java
@@ -0,0 +1,20 @@
+package edu.berkeley.capstoneproject.capstoneprojectandroid.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import edu.berkeley.capstoneproject.capstoneprojectandroid.R;
+
+/**
+ * Created by Alex on 19/10/2017.
+ */
+
+public class HttpDemoActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_demo);
+    }
+}
